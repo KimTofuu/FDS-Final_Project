4417c0b1c7aa96c63da438712c70f9423aa84959
SioPauwii <paulocordova3@gmail.com>
2024-09-15 00:04:39 +0800

    <3

--- Backend/routes/UserLogin.php
+++ Backend/routes/UserLogin.php
@@ -14,42 +14,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if (empty($username) || empty($password)) echo("All fields are required");
+    if (empty($username) || empty($password)){ 
+        echo("All fields are required"); 
+    }
+    else {
+        $sqlComm = "SELECT Password FROM `main` WHERE Username = ?  &&  Status = 1;";
+            
+            if ($statement = $connect->prepare($sqlComm)){
+                $statement->bind_param("s", $username);
+                $statement->execute();
+                $statement->store_result();
 
-    $sqlComm = "SELECT Password FROM `main` WHERE Username = ?;";
-    
-    if ($statement = $connect->prepare($sqlComm)){
-        $statement->bind_param("s", $username);
-        $statement->execute();
-        $statement->store_result();
+                if($statement->num_rows>0){
+                    $statement->bind_result($hashed_password);
+                    $statement->fetch();
 
-        if($statement->num_rows>0){
-            $statement->bind_result($hashed_password);
-            $statement->fetch();
-
-            if(password_verify($password, $hashed_password)){
-                echo "Access Granted! Welcome!";
-            }else{
-                echo "Invalid Username and Password. Please try again.";
+                    if(password_verify($password, $hashed_password)){
+                        echo "Access Granted! Welcome!";
+                    }else{
+                        echo "Invalid Username or Password. Please try again.";
+                        }
                 }
-        }
-        else{
-            echo "Invalid Username and Password. Please try again.";
-        }
-        $statement->close();
+                else {
+                    echo "Invalid Username or Password. Please try again.";
+                }
+                $statement->close();
+            }
+            $connect->close();
+        } 
     }
-    else{
-        echo "Invalid Username and Password. Please try again.";
-    }
-    $connect->close();
-    } 
 ?>
     <form method="post">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" placeholder="Enter Username or Email">
         
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password"` pattern=".{8,}" placeholder="Eight or more characters">
+        <input type="password" id="password" name="password"` placeholder="Eight or more characters">
         
         <button type="submit" name="login">Log in</button>
 </form>
